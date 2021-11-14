ce89e803236dd68e7c6f5c00bea05fe1cb7ff9f7
Ninja-Nevaan <84234995+Ninja-Nevaan@users.noreply.github.com>
2021-11-14 12:16:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var name1="Nevaan Gupta"
+console.log(name1)
+
+var max_num=Math.max(6,8,10,12)
+console.log(max_num)
+
+var min_num=Math.min(6,8,10,12)
+console.log(min_num)
+
+var exponent=Math.pow(4,2)
+console.log(exponent)
+
+var exponet2=Math.pow(3,40)
+console.log(exponet2)
+
+var big_number=3.141592653
+console.log(big_number)
+
+var pi=big_number.toFixed(2)
+console.log(pi)
+
+var n1=8;
+var n2=80;
+console.log(n1+n2)
+
+var n1_str=n1.toString();
+var n2_str=n2.toString();
+console.log(n1_str+n2_str)
+
+var n3=888;
+n3++
+//
+console.log(n3)
